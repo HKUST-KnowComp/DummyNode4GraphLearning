f52b7f4eafad9e239bb36500f4dd415179087024
Jiayang Cheng <49507817+loginaway@users.noreply.github.com>
2022-08-01 14:39:49 +0800

    [Update] add relational conv models

--- graph_classification/graph_neural_networks/models/gconv.py
+++ graph_classification/graph_neural_networks/models/gconv.py
@@ -32,17 +32,6 @@ class GCN(torch.nn.Module):
             self.use_edge_weight = True
         else:
             self.use_edge_weight = False
-            
-        # additional transformation for nodes
-        self.use_transform_dummy = False
-        if args.transform_dummy != '':
-            self.use_transform_dummy = True
-            self.lin_dummy = [torch.nn.Linear(self.hidden_dim, self.hidden_dim)]
-            if args.transform_dummy == 'no_activation':
-                pass
-            else:
-                self.lin_dummy.append(torch.nn.ReLU())
-            self.lin_dummy = torch.nn.Sequential(*self.lin_dummy)
                 
         self.conv1 = GCNConv(self.num_features, self.hidden_dim)
         self.conv2 = GCNConv(self.hidden_dim, self.hidden_dim)
@@ -60,12 +49,6 @@ class GCN(torch.nn.Module):
             edge_attr[data.is_dummy_edge.to(self.args.device)] = self.dummy_weight
             
         x = F.relu(self.conv1(x, edge_index, edge_attr))
-        # additional transformation for nodes
-        if self.use_transform_dummy:
-            # the last node in each graph is the dummy node (in that graph)
-            index_last_in_graph = get_last_index_in_graph(data)
-            x[index_last_in_graph] = self.lin_dummy(x[index_last_in_graph])
-
         x = F.relu(self.conv2(x, edge_index, edge_attr))
         x = gap(x, batch)
 
@@ -92,17 +75,6 @@ class GCN_concat_readout(torch.nn.Module):
         else:
             self.use_edge_weight = False
             
-        # additional transformation for nodes
-        self.use_transform_dummy = False
-        if args.transform_dummy != '':
-            self.use_transform_dummy = True
-            self.lin_dummy = [torch.nn.Linear(self.hidden_dim, self.hidden_dim)]
-            if args.transform_dummy == 'no_activation':
-                pass
-            else:
-                self.lin_dummy.append(torch.nn.ReLU())
-            self.lin_dummy = torch.nn.Sequential(*self.lin_dummy)
-                
         self.conv1 = GCNConv(self.num_features, self.hidden_dim)
         self.conv2 = GCNConv(self.hidden_dim, self.hidden_dim)
         
@@ -119,12 +91,6 @@ class GCN_concat_readout(torch.nn.Module):
             edge_attr[data.is_dummy_edge.to(self.args.device)] = self.dummy_weight
             
         x = F.relu(self.conv1(x, edge_index, edge_attr))
-        # additional transformation for nodes
-        if self.use_transform_dummy:
-            # the last node in each graph is the dummy node (in that graph)
-            index_last_in_graph = get_last_index_in_graph(data)
-            x[index_last_in_graph] = self.lin_dummy(x[index_last_in_graph])
-        
         x = F.relu(self.conv2(x, edge_index, edge_attr))
         x = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
         
@@ -149,8 +115,7 @@ class GraphSAGE(torch.nn.Module):
         self.dropout_ratio = args.dropout_ratio
         
         if args.additional:
-            import json
-            config = json.loads(args.additional) # "
+            config = args.additional
         else:
             config = {'num_layers': 2,
                       'aggregation': 'mean'}
@@ -159,24 +124,6 @@ class GraphSAGE(torch.nn.Module):
         num_layers = config.get('num_layers', 2)
         self.aggregation = config.get('aggregation', 'mean')
         
-        # dummy edge weight
-        if args.dummy_weight > 0:
-            self.dummy_weight = torch.tensor(args.dummy_weight, requires_grad=True, device=self.args.device)
-            self.use_edge_weight = True
-        else:
-            self.use_edge_weight = False
-            
-        # additional transformation for nodes
-        self.use_transform_dummy = False
-        if args.transform_dummy != '':
-            self.use_transform_dummy = True
-            self.lin_dummy = [torch.nn.Linear(self.hidden_dim, self.hidden_dim)]
-            if args.transform_dummy == 'no_activation':
-                pass
-            else:
-                self.lin_dummy.append(torch.nn.ReLU())
-            self.lin_dummy = torch.nn.Sequential(*self.lin_dummy)
-                
         self.layers = nn.ModuleList([])
         for i in range(num_layers):
             dim_input = self.num_features if i==0 else self.hidden_dim
@@ -189,16 +136,11 @@ class GraphSAGE(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-        edge_attr = None
-        if self.use_edge_weight:
-            # when trainable dummy edge weight is enabled
-            edge_attr = torch.ones(data.is_dummy_edge.size()).to(self.args.device)
-            edge_attr[data.is_dummy_edge.to(self.args.device)] = self.dummy_weight
         
         # SAGE forward
         x_all = []
         for i, layer in enumerate(self.layers):
-            x = layer(x, edge_index, edge_attr)
+            x = layer(x, edge_index)
             if self.aggregation == 'max':
                 x = torch.relu(self.fc_max(x))
             x_all.append(x)
@@ -225,8 +167,7 @@ class GIN(torch.nn.Module):
         self.dropout = args.dropout_ratio
         
         if args.additional:
-            import json
-            config = json.loads(args.additional) 
+            config = args.additional
         else:
             config = {'train_eps': False,
                       'num_layers': 2,
@@ -236,25 +177,7 @@ class GIN(torch.nn.Module):
         elif config.get('aggregation', 'sum') == 'mean':
             self.pooling = global_mean_pool
         train_eps = config.get('train_eps', args.epochs)
-        
-        # dummy edge weight
-        if args.dummy_weight > 0:
-            self.dummy_weight = torch.tensor(args.dummy_weight, requires_grad=True, device=self.args.device)
-            self.use_edge_weight = True
-        else:
-            self.use_edge_weight = False
-            
-        # additional transformation for nodes
-        self.use_transform_dummy = False
-        if args.transform_dummy != '':
-            self.use_transform_dummy = True
-            self.lin_dummy = [torch.nn.Linear(self.hidden_dim, self.hidden_dim)]
-            if args.transform_dummy == 'no_activation':
-                pass
-            else:
-                self.lin_dummy.append(torch.nn.ReLU())
-            self.lin_dummy = torch.nn.Sequential(*self.lin_dummy)
-        
+
         self.embeddings_dim = [self.hidden_dim for _ in range(config.get('num_layers', 2))]
         self.no_layers = len(self.embeddings_dim)
         self.first_h = []
@@ -279,11 +202,6 @@ class GIN(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-        edge_attr = None
-        if self.use_edge_weight:
-            # when trainable dummy edge weight is enabled
-            edge_attr = torch.ones(data.is_dummy_edge.size()).to(self.args.device)
-            edge_attr[data.is_dummy_edge.to(self.args.device)] = self.dummy_weight
         
         out = 0
         for layer in range(self.no_layers):
@@ -291,7 +209,7 @@ class GIN(torch.nn.Module):
                 x = self.first_h(x)
                 out += F.dropout(self.pooling(self.linears[layer](x), batch), p=self.dropout)
             else:
-                x = self.convs[layer-1](x, edge_index, edge_attr)
+                x = self.convs[layer-1](x, edge_index)
                 out += F.dropout(self.linears[layer](self.pooling(x, batch)), p=self.dropout, training = self.training)
         out = F.log_softmax(out, dim=-1)
         return out
